aff6cb34e67812eaaf7862d261f9289f3ccee1b9
Bekzhan <bekzhan.tanirbergen@narxoz.kz>
2026-03-02 15:15:43 +0500

    update Main

--- src/com/narxoz/rpg/Main.java
+++ src/com/narxoz/rpg/Main.java
@@ -5,6 +5,7 @@ import com.narxoz.rpg.adapter.HeroCombatantAdapter;
 import com.narxoz.rpg.battle.BattleEngine;
 import com.narxoz.rpg.battle.Combatant;
 import com.narxoz.rpg.battle.EncounterResult;
+import com.narxoz.rpg.enemy.BasicEnemy;
 import com.narxoz.rpg.enemy.Goblin;
 import com.narxoz.rpg.hero.Mage;
 import com.narxoz.rpg.hero.Warrior;
@@ -16,26 +17,28 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== RPG Battle Engine Demo ===\n");
 
-        // TODO: Create heroes and enemies
         Warrior warrior = new Warrior("Arthas");
         Mage mage = new Mage("Jaina");
         Goblin goblin = new Goblin();
 
-        // TODO: Wrap with adapters
+        Goblin goblin = new Goblin();
+        BasicEnemy orc = new BasicEnemy("Orc Brute", 18, 95);
+        BasicEnemy skeleton = new BasicEnemy("Skeleton Archer", 14, 70);
+
         List<Combatant> heroes = new ArrayList<>();
         heroes.add(new HeroCombatantAdapter(warrior));
         heroes.add(new HeroCombatantAdapter(mage));
 
         List<Combatant> enemies = new ArrayList<>();
         enemies.add(new EnemyCombatantAdapter(goblin));
+        enemies.add(new EnemyCombatantAdapter(orc));
+        enemies.add(new EnemyCombatantAdapter(skeleton));
 
-        // TODO: Demonstrate Singleton behavior
         BattleEngine engineA = BattleEngine.getInstance();
         BattleEngine engineB = BattleEngine.getInstance();
         System.out.println("Same instance? " + (engineA == engineB));
         System.out.println();
 
-        // TODO: Run battle and print summary
         engineA.setRandomSeed(42L);
         EncounterResult result = engineA.runEncounter(heroes, enemies);
 
